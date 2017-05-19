cc244a7e1b2857b64959c69b9221100a59ed3c46
csanahuja <csanahuja10@gmail.com>
2017-05-19 18:10:43 +0200

    Modified Model Class to be called by function

--- src/main/scala/Learning/MP/MPTraining.scala
+++ src/main/scala/Learning/MP/MPTraining.scala
@@ -9,59 +9,33 @@ import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.classification.MultilayerPerceptronClassificationModel;
 
-// @Annotation
-// TODO: rework program to just generate 1 model by the parameters entered
-// as arguments. Make a script to call the program with different parameters.
-// Study the influence of different parameters and see which one are more succes.
-
 // MultilayerPerceptronTraining
-object MPT {
+class MPT(ss: SparkSession, input: String) {
 
-  class Params(var input : String, var output : String, var maxIters : Int,
-               var block: Int, var seed: Long, var layers : Array[Int]) {
-    def this() {
-        this("", "", 100, 128, 1234L, Array[Int]());
-    }
-    override def toString : String =
-      "Params: \n" +
-      " - Input =    " + input + "\n" +
-      " - Output =   " + output + "\n" +
-      " - MaxIters = " + maxIters + "\n" +
-      " - Block =    " + block + "\n" +
-      " - Seed =     " + seed + "\n" +
-      " - Layers =   " + layers + "\n"
+  // Load the data stored in LIBSVM format as a DataFrame.
+  val data = ss.read.format("libsvm").load(input)
 
-  }
+  class Params(var training_size : Double = 0.75, var maxIters : Int = 100, var seed: Long = 1234L,
+               var block: Int = 128, var layers : Array[Int] = Array.empty[Int])
 
-  def main(args: Array[String]) {
-    val sc = SparkSession
-      .builder
-      .appName("Multilayer Perceptron Model")
-      .getOrCreate()
+  def generateModel(in_params: Params, output_file: String = "",
+                    random: Boolean = false, accuracy_log : Boolean = false) {
 
-    //Parse and Save Params
-    val arglist = args.toList
-    val params = new Params()
-    getOptions(arglist, params)
 
-    if(params.input == ""){
-      val msg = """Usage -> arguments: -arg value
-                  argument: -input required""".stripMargin
-      println(msg)
-      sys.exit(1)
-    }
-
-    // Load the data stored in LIBSVM format as a DataFrame.
-    val data = sc.read.format("libsvm").load(params.input)
-
-    // Split the data into train and test
-    val splits = data.randomSplit(Array(0.75, 0.25))
-    val (train, test) = (splits(0), splits(1))
-
-    // Parse params
+    // Set Layers
     val numFeatures = data.first().getAs[Vector](1).size
     val classes = 3
-    parseParams(params, numFeatures, classes)
+
+    var params = in_params
+    setLayer(params, numFeatures, classes)
+
+    if (random)
+      getRandomParams(params)
+
+
+    // Split the data into train and test
+    val splits = data.randomSplit(Array(params.training_size, 1-params.training_size))
+    val (train, test) = (splits(0), splits(1))
 
     // Generate Model
     val trainer = new MultilayerPerceptronClassifier()
@@ -73,50 +47,30 @@ object MPT {
 
     // Get Accuracy
     val accuracy = getAccuracy(model, test)
-    printf("Accuracy: " + accuracy + "\n")
+    if (accuracy_log)
+       printf("Accuracy: " + accuracy + "\n")
+
 
     // Save model
-    if(params.output == "")
+    if(output_file == "")
       model.write.overwrite().save("target/tmp/MPM")
     else
-      model.write.overwrite().save(params.output)
+      model.write.overwrite().save(output_file)
 
-    sc.stop()
+    ss.stop()
     }
 
-    def getOptions(list: List[String], params: Params){
-      def isSwitch(s : String) = (s(0) == '-')
-      list match {
-        case Nil =>
-        case "-input"  :: value :: tail => params.input = value.toString
-                                           getOptions(tail, params)
-        case "-output" :: value :: tail => params.output = value.toString
-                                           getOptions(tail, params)
-        case "-max"    :: value :: tail => params.maxIters = value.toInt
-                                           getOptions(tail, params)
-        case "-block"  :: value :: tail => params.block = value.toInt
-                                           getOptions(tail, params)
-        case "-seed"   :: value :: tail => params.seed = value.toLong
-                                           getOptions(tail, params)
-        case "-layers" :: value :: tail => params.layers = value.split(",").toArray.map(_.toString.toInt)
-                                           getOptions(tail, params)
-        case option :: tail => println("Unknown option " + option)
-                               sys.exit(1)
-      }
-
+    // Set to the layers the features and classes
+    def setLayer(params: Params, num_features : Int, output_classes: Int){
+      params.layers = Array(num_features) ++ params.layers ++ Array(output_classes)
     }
 
-    def parseParams(params: Params, num_features : Int, output_classes: Int){
-      //layers
-      if (params.layers.size == 0)
-        params.layers = getRandomLayer(num_features, output_classes)
-      else
-        params.layers = Array(num_features) ++ params.layers ++ Array(output_classes)
-
-      //seed
-      params.seed = System.currentTimeMillis()
+    // Dummy Sample
+    def getRandomParams(params: Params){
+      var i = 5
     }
 
+    // Generates random layers
     def getRandomLayer(num_features : Int, output_classes: Int): Array[Int] = {
       val r = Random
       var num_layers = r.nextInt(5)
@@ -136,7 +90,7 @@ object MPT {
       return layers
     }
 
-    //Auxiliar Function: returns accuracy of a given model with a data set
+    // Returns accuracy of a given model with a data set
     def getAccuracy(model: MultilayerPerceptronClassificationModel,
                       test: DataFrame): Double = {
       //compute accuracy on the test set
